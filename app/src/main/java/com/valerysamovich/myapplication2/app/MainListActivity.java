fd924e83bae0eb686f66f2fc30ac808487d6246a
SAMOV004 <valery.y.samovich@disney.com>
2014-05-20 00:43:21 -0400

    Create the list activity

--- app/src/main/java/com/valerysamovich/myapplication2/app/MainListActivity.java
+++ app/src/main/java/com/valerysamovich/myapplication2/app/MainListActivity.java
@@ -1,31 +1,17 @@
 package com.valerysamovich.myapplication2.app;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
+import android.app.ListActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
 
-
-
-public class MainListActivity extends ActionBarActivity {
+public class MainListActivity extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_list);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -46,19 +32,4 @@ public class MainListActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main_list, container, false);
-            return rootView;
-        }
-    }
 }
