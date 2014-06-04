2dd35393f2b962a7d8c0aac846678ffa17af068d
SAMOV004 <valerysamovich@gmail.com>
2014-06-04 01:43:14 -0400

    Add new BlogWebViewActivity

--- app/src/main/java/com/valerysamovich/app/MainListActivity.java
+++ app/src/main/java/com/valerysamovich/app/MainListActivity.java
@@ -88,7 +88,8 @@ public class MainListActivity extends ListActivity {
             JSONArray jsonPosts = mBlogData.getJSONArray("posts");
             JSONObject jsonPost = jsonPosts.getJSONObject(position);
             String blogUrl = jsonPost.getString("url");
-            Intent intent = new Intent(Intent.ACTION_VIEW);
+            // Explicit intent
+            Intent intent = new Intent(this, BlogWebViewActivity.class);
             intent.setData(Uri.parse(blogUrl));
             startActivity(intent);
         } catch (JSONException e) {
@@ -98,7 +99,7 @@ public class MainListActivity extends ListActivity {
 
     /**
      * Logging the exception
-     * @param e
+     * @param e exception variable
      */
     private void logException(Exception e) {
         Log.e(TAG, "Exception caught!", e);
